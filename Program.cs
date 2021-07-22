7450aa247c7846afa78f4c20c7ce0cd1dbeca2de
dmannix <dmannix1103@gmail.com>
2021-07-22 19:07:37 +1000

    using very big primes now

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace elgamal
             string message = "Now is the winter of our discontent.";
 	    byte[] m = Encoding.ASCII.GetBytes(message);
 	    BigInteger mod = BigInteger.Parse("96398551441758641432498106966804152150608120353342563371292746847062383495952333903880077300033579628752765930551257877420508008817301766932673738933713818991130871127607526293923392284520225229071937");
-	    BigInteger g = BigInteger.Parse("397589");
+	    BigInteger g = BigInteger.Parse("846421440089630429815989955158439699330579");
 	    ElGamalCipher elg = new ElGamalCipher(mod, g);
 	    BigInteger c1, c2;
 	    (c1,c2) = elg.Encrypt(m);
